f9dc44cf9f26154e0d130c7d922fb1352c802abb
Andrew Lisowski <lisowski54@gmail.com>
2018-12-02 17:34:04 -0800

    run prettier

--- src/browser/page2layers.js
+++ src/browser/page2layers.js
@@ -9,56 +9,68 @@ const getNodeName = node =>
 
 const fixPseudoElements = () => {
   // Hide old pseudo-elements during render
-  const css = '.before-reset::before, .after-reset::after { content: none !important; }';
-  const head = document.head || document.getElementsByTagName('head')[0];
-  const style = document.createElement('style');
+  const css =
+    ".before-reset::before, .after-reset::after { content: none !important; }";
+  const head = document.head || document.getElementsByTagName("head")[0];
+  const style = document.createElement("style");
 
-  style.type = 'text/css';
+  style.type = "text/css";
   style.appendChild(document.createTextNode(css));
   head.appendChild(style);
 
-  const allElements = document.querySelectorAll('body *');
-  const oldFakes = document.querySelectorAll('body .fake-pseudo');
+  const allElements = document.querySelectorAll("body *");
+  const oldFakes = document.querySelectorAll("body .fake-pseudo");
 
   // Remove old fake pseudo-elements to handle multiple screen sizes
   if (oldFakes) {
-    Array.from(oldFakes).map(el => el.remove())
+    Array.from(oldFakes).map(el => el.remove());
   }
 
   for (let i = 0; i < allElements.length; i++) {
-    // Remove reset so we can get the screen sizes pseudo-element styles 
-    if ( allElements[i].className){
-      allElements[i].className = allElements[i].className.replace('before-reset', '')
-      allElements[i].className = allElements[i].className.replace('after-reset', '')
+    // Remove reset so we can get the screen sizes pseudo-element styles
+    if (allElements[i].className) {
+      allElements[i].className = allElements[i].className.replace(
+        "before-reset",
+        ""
+      );
+      allElements[i].className = allElements[i].className.replace(
+        "after-reset",
+        ""
+      );
     }
-    
 
-    const elementBeforeStyles = window.getComputedStyle(allElements[i], ':before');
-    const elementAfterStyles = window.getComputedStyle(allElements[i], ':after');
+    const elementBeforeStyles = window.getComputedStyle(
+      allElements[i],
+      ":before"
+    );
+    const elementAfterStyles = window.getComputedStyle(
+      allElements[i],
+      ":after"
+    );
     const elementBeforeContent = elementBeforeStyles.content;
     const elementAfterContent = elementAfterStyles.content;
 
-    if (elementBeforeContent && elementBeforeContent !== 'none') {
-      const virtualBefore = document.createElement('span');
+    if (elementBeforeContent && elementBeforeContent !== "none") {
+      const virtualBefore = document.createElement("span");
 
-      virtualBefore.className = "fake-pseudo"
-      virtualBefore.setAttribute('style', elementBeforeStyles.cssText);
-      virtualBefore.innerHTML = elementBeforeStyles.content.split('"').join('');
-      allElements[i].className += ' before-reset';
+      virtualBefore.className = "fake-pseudo";
+      virtualBefore.setAttribute("style", elementBeforeStyles.cssText);
+      virtualBefore.innerHTML = elementBeforeStyles.content.split('"').join("");
+      allElements[i].className += " before-reset";
       allElements[i].prepend(virtualBefore);
     }
 
-    if (elementAfterContent && elementAfterContent !== 'none') {
-      const virtualAfter = document.createElement('span');
+    if (elementAfterContent && elementAfterContent !== "none") {
+      const virtualAfter = document.createElement("span");
 
-      virtualAfter.className = "fake-pseudo"    
-      virtualAfter.setAttribute('style', elementAfterStyles.cssText);
-      virtualAfter.innerHTML = elementAfterStyles.content.split('"').join('');
-      allElements[i].className += ' after-reset';
+      virtualAfter.className = "fake-pseudo";
+      virtualAfter.setAttribute("style", elementAfterStyles.cssText);
+      virtualAfter.innerHTML = elementAfterStyles.content.split('"').join("");
+      allElements[i].className += " after-reset";
       allElements[i].appendChild(virtualAfter);
     }
   }
-}
+};
 
 export const getSymbol = ({
   name = "symbol",
